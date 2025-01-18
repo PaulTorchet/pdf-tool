020fb23331a93b6cbc082a3b7077bab385d838d5
Paul TORCHET <ptorchet.dev@gmail.com>
2025-01-18 02:00:50 +0100

    Improve CLI Help

--- pdf_tool/cli.py
+++ pdf_tool/cli.py
@@ -21,18 +21,20 @@ def cli():
     pass
 
 
-@cli.command(aliases=["i"])
+@cli.command(aliases=["i"], no_args_is_help=True)
 @click.argument("file", type=click.Path(exists=True, dir_okay=False))
 @click.option("--output-json", "--json", is_flag=True, default=False, help="Output data as JSON instead of table.")
+@click.help_option("-h", "--help")
 def info(file, output_json):
     """Display PDF info."""
     display_pdf_info(file_path=file, output_json=output_json)
 
 
-@cli.command(aliases=["cs"])
+@cli.command(aliases=["cs"], no_args_is_help=True)
 @click.argument("file", type=click.Path(exists=True, dir_okay=False))
 @click.option("--output", "-o", type=click.Path(exists=False, dir_okay=False), help="Output file.")
 @click.option("--contrast", "-c", type=float, default=2, help="Contrast ratio. Default to 2.")
+@click.help_option("-h", "--help")
 def contrast(file, output, contrast):
     """Increase PDF contrast."""
 
@@ -42,11 +44,12 @@ def contrast(file, output, contrast):
     change_pdf_contrast(pdf_path=file, output_path=output, contrast=contrast)
 
 
-@cli.command(aliases=["sr"])
+@cli.command(aliases=["sr"], no_args_is_help=True)
 @click.argument("file", type=click.Path(exists=True, dir_okay=False))
 @click.argument("ranges", type=str, required=True, nargs=-1, callback=validate_ranges)
 @click.option("--destination", "-d", type=click.Path(exists=False, dir_okay=True), help="Output directory.")
 @click.option("--name", "-n", type=str, help="Output file name.")
+@click.help_option("-h", "--help")
 def split_range(file, ranges, destination, name):
     """Split PDF by ranges."""
 
@@ -63,11 +66,12 @@ def split_range(file, ranges, destination, name):
         file_path=file, destination=destination, output_name=name, ranges=ranges)
 
 
-@cli.command(aliases=["si"])
+@cli.command(aliases=["si"], no_args_is_help=True)
 @click.argument("file", type=click.Path(exists=True, dir_okay=False))
 @click.option("--interval", "-i", type=int, default=1, help="Interval used to split PDF. Default to 1.")
 @click.option("--destination", "-d", type=click.Path(exists=False, dir_okay=True), help="Output directory.")
 @click.option("--name", "-n", type=str, help="Output file name.")
+@click.help_option("-h", "--help")
 def split_interval(file, interval, destination, name):
     """Split PDF by interval."""
 
@@ -84,10 +88,11 @@ def split_interval(file, interval, destination, name):
         file_path=file, destination=destination, output_name=name, interval=interval)
 
 
-@cli.command(aliases=["r"])
+@cli.command(aliases=["r"], no_args_is_help=True)
 @click.argument("file", type=click.Path(exists=True, dir_okay=False))
 @click.argument("order", type=str, required=True, nargs=-1, callback=validate_order)
 @click.option("--output", "-o", type=click.Path(exists=False, dir_okay=False), help="Output file.")
+@click.help_option("-h", "--help")
 def reorganize(file, order, output):
     """Reorganize PDF pages."""
 
